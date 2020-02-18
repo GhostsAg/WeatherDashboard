c44bcf37de7146e7e32bde71ac5487d29b9b8c1b
GhostsAg <Andres.Ledesma9511@gmail.com>
2020-02-18 00:12:45 -0600

    Added logic and api call, formatting to be done

--- assets/logic.js
+++ assets/logic.js
@@ -2,20 +2,19 @@
 $(document).ready(function() {
     // ajax request vars
     var cityVar = "";
-    var lat = "";
-    var lon = "";
     const api_key = "bfa2edb3854eb69c605b1ee877b198e2";
     var weatherURL;
     var forecastURL;
     var date = moment().format('MMM Do YY');
+    var history;
     
     // index var for local storage
     var cityIndex;
 
     // elements selectors
-    var history;
+    var $forecast = document.querySelectorAll(".card");
 
-
+    // Functions 
     function loadHist() {
         if (!localStorage.getItem("index")) {
             cityIndex = -1;
@@ -25,7 +24,7 @@ $(document).ready(function() {
             $("#searchHistory").empty();
             cityIndex = localStorage.getItem("index");
             for (let i=-1; i<cityIndex; i++) {
-                $("#searchHistory").append(`<li>${localStorage.getItem(`City: ${i+1}`)}</li><hr>`);
+                $("#searchHistory").prepend(`<li>${localStorage.getItem(`City: ${i+1}`)}</li><hr>`);
             }
         }
     }
@@ -34,6 +33,25 @@ $(document).ready(function() {
         if (history==false) {
             $("#searchHistory").empty();
             history = true;
+        }
+    }
+
+    function icons() {
+        // Clear, Clouds, Rain, 
+        for (let i=0; i<$forecast.length; i++) {
+            var icon = $forecast[i].children[1].getAttribute('data-name');
+            if (icon=="Clear") {
+                $forecast[i].children[1].setAttribute("class", "fas fa-cloud-sun");
+            }
+            else if (icon == "Clouds") {
+                $forecast[i].children[1].setAttribute("class", "fas fa-cloud");
+            }
+            else if (icon == "Rain") {
+                $forecast[i].children[1].setAttribute("class", "fas fa-cloud-rain");
+            }
+            else {
+                $forecast[i].children[1].setAttribute("class", "fas fa-wrench");  
+            }
         }
     }
 
@@ -50,16 +68,18 @@ $(document).ready(function() {
         localStorage.setItem(`index`, `${cityIndex}`);
         var $newCity = $(`<li>${cityVar}</li>`);
         var $hr = $("<hr>");
-        $("#searchHistory").append($newCity);
-        $("#searchHistory").append($hr);
+        $("#searchHistory").prepend($hr);
+        $("#searchHistory").prepend($newCity);
+        
+        // coords
+        var lat = "";
+        var lon = "";
 
         // API calls.
         $.ajax({
             url: weatherURL 
         })
         .then(function(response) {
-            console.log(response);
-            console.log(response.coord.lat);
             lon = response.coord.lon;
             lat = response.coord.lat;
             $("#dispCity").text(`${response.name}  (${date})`);
@@ -68,15 +88,31 @@ $(document).ready(function() {
             $("#temp").text(`Temperature:  ${tempF}\u00B0F / ${tempC}\u00B0C`);
             $("#humidity").text(`Humidity:  ${response.main.humidity}%`);
             $("#windSpeed").text(`Wind Speed:  ${response.wind.speed} MPH`);
-        });
 
-        uviURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${api_key}&lon=${lon}&lat=${lat}`; 
-        $.ajax({
-            url: uviURL 
-        })
-        .then(function(response) {
-            console.log(response);
-            $("#uv").text(`UV Index:  ${reponse.value}`);
+            uviURL = `https://api.openweathermap.org/data/2.5/uvi?appid=${api_key}&lon=${lon}&lat=${lat}`; 
+            $.ajax({
+                url: uviURL 
+            })
+            .then(function(newResponse) {
+                var uvi = newResponse.value;
+                $("#uv").text(uvi);
+                if (uvi <= 2) {
+                    $("#uv").css("background-color", "green");
+                    $("#uv").css("color", "black");
+                }
+                else if (uvi <= 5) {
+                    $("#uv").css("background-color", "yellow");
+                    $("#uv").css("color", "black");
+                }
+                else if (uvi <= 7) {
+                    $("#uv").css("background-color", "orange");
+                }
+                else if (uvi <= 10) {
+                    $("#uv").css("background-color", "red");
+                }else {
+                    $("#uv").css("background-color", "purple");
+                }
+            });
         });
     
         $.ajax({
@@ -84,8 +120,15 @@ $(document).ready(function() {
         })
         .then(function(response) {
             console.log(response);
+            for (let i=0; i<$forecast.length; i++) {
+                $forecast[i].children[0].textContent = moment().add(i+1, "days").format('MMM Do YY');
+                $forecast[i].children[1].setAttribute("data-name", response.list[i*8].weather[0].main);
+                var temp = ((response.list[i*8].main.temp - 273.15) * 9/5 + 32).toPrecision(2);
+                var humidity = response.list[i*8].main.humidity;
+                $forecast[i].children[2].textContent = `Temp: ${temp}\u00B0F`;
+                $forecast[i].children[3].textContent = `Humidity: ${humidity}%`;
+            }
+            icons();
         });
     });
-
-    
 })
